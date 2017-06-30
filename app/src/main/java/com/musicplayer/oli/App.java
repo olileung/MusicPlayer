7adaa59c385690bdfb27965d755f4abf8857208e
oliver <oliver@digi.me>
2017-06-30 12:48:15 +0100

    add quit button and terms lorem

--- app/src/main/java/com/musicplayer/oli/App.java
+++ app/src/main/java/com/musicplayer/oli/App.java
@@ -1,16 +1,14 @@
 package com.musicplayer.oli;
 
 import android.app.Application;
-import android.util.Log;
 
 /**
  * Created by Oli on 29/06/2017.
  */
 
-public class App extends Application{
+public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("Oli", "Test message");
     }
 }
--- app/src/main/java/com/musicplayer/oli/LoginActivity.java
+++ app/src/main/java/com/musicplayer/oli/LoginActivity.java
@@ -7,22 +7,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import static com.musicplayer.oli.R.id.login_quit_button;
+
 /**
  * Created by Oli on 29/06/2017.
  */
 
 public class LoginActivity extends AppCompatActivity {
     private Button login;
+    private Button quit;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         login = (Button) findViewById(R.id.login_login_button);
+        quit = (Button) findViewById(login_quit_button);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 loginSuccess();
+            }
+        });
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quitApp();
             }
         });
     }
@@ -30,5 +40,9 @@ public class LoginActivity extends AppCompatActivity {
     private void loginSuccess() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    private void quitApp() {
+        finish();
     }
 }
